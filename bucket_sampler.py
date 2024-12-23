7182a891c15223e3bb239a5441aa810fbadfa496
LucasMaloBelanger <lucas.malo.belanger@gmail.com>
2024-12-23 15:24:57 -0500

    Initial commit

---
+++ bucket_sampler.py
@@ -0,0 +1,239 @@
+import multiprocessing
+from tqdm import tqdm
+from torch.utils.data import BatchSampler, IterableDataset
+import random
+from copy import copy
+from torchvision.transforms import Resize, Compose, RandomCrop, RandomHorizontalFlip, CenterCrop, PILToTensor
+import torch
+
+def find_closest_ratio(idx, img, aspect_ratios):
+    width = img.width
+    height = img.height
+    ratio = height / width
+
+    # find the closest ratio from the aspect ratios table
+    min_distance = 100
+    target_ratio = 0.6
+    for r in aspect_ratios.keys():
+        distance = abs(float(r) - ratio)
+        if min_distance > distance:
+            target_ratio = r
+            min_distance = distance
+    
+    # Return the result as a tuple (target_ratio, idx)
+    return str(target_ratio), idx
+
+def get_bucket_indices(dataset, aspect_ratios, batch_size):
+    buckets = {}
+    idx = 0
+    for img, caption in tqdm(dataset, desc='Calculating buckets...'):
+        ratio, idx = find_closest_ratio(idx, img, aspect_ratios)
+        if ratio not in buckets.keys():
+            buckets[ratio] = []
+        buckets[ratio].append(idx)
+        idx = idx + 1
+
+    # create batches out of that
+    indices = []
+    batch = []
+    for key, value in tqdm(buckets.items(), desc='Calculating reordered dataset'):
+        value_copy = copy(value)
+        random.shuffle(value_copy)
+
+        for idx in value_copy:
+            batch.append(idx)
+            indices.append(idx)
+
+            if len(batch) == batch_size:
+                batch = []
+        if batch != []:
+            while len(batch) != batch_size:
+                idx = random.choice(value_copy)
+                batch.append(idx)
+                indices.append(idx)
+            batch = []
+    return indices
+
+class BucketSampler(BatchSampler):
+    def __init__(self,
+                 dataset,
+                 aspect_ratios,
+                 num_processes,
+                 batch_size):
+        random.seed(0)
+
+        self.buckets = {}
+        self.aspect_ratios = aspect_ratios
+        self.batch_size = batch_size
+
+        idx = 0
+        for img, caption in tqdm(dataset, desc='Calculating buckets...'):
+            ratio, idx = find_closest_ratio(idx, img, self.aspect_ratios)
+            if ratio not in self.buckets.keys():
+                self.buckets[ratio] = []
+            self.buckets[ratio].append(idx)
+            idx = idx + 1 
+    
+    def __iter__(self):
+        batch = []
+        for key, value in self.buckets.items():
+            value_copy = copy(value)
+            random.shuffle(value_copy)
+
+            for idx in value_copy:
+                batch.append(idx)
+
+                if len(batch) == self.batch_size:
+                    yield batch
+                    batch = []
+            if batch != []:
+                while len(batch) != self.batch_size:
+                    idx = random.choice(value_copy)
+                    batch.append(idx)
+                yield batch
+                batch = []
+    
+    def __len__(self):
+        return int(self.num_images // self.batch_size)
+
+class BatchCollater:
+    def __init__(self, aspect_ratios):
+        self.aspect_ratios = aspect_ratios
+
+    def __call__(self, *args, **kwds):
+        samples = args[0]
+        images = []
+        captions = []
+
+        # choose the closest ratio from the first image
+        img, caption = samples[0]
+        closest_ratio, idx = find_closest_ratio(0, img, self.aspect_ratios)
+        height_target = int(self.aspect_ratios[closest_ratio][0])
+        width_target = int(self.aspect_ratios[closest_ratio][1])
+
+        # define a transformation so we get all images with the size of the first image in the batch
+        crop_transform = CenterCrop((height_target, width_target))
+        pil_to_tensor = PILToTensor()
+
+        for sample in samples:
+            img, caption = sample
+            captions.append(caption)
+
+            # this is bad if the image is low resolution
+                    # resize the image so it fits best the target
+            width = img.width
+            height = img.height
+
+            if width > height:
+                ratio = width_target / width
+                new_height = height * ratio
+                new_width = width_target
+            else:
+                ratio = height_target / height
+                new_width = width * ratio
+                new_height = height_target
+            transform = Resize((int(new_height), int(new_width)))
+
+            img = transform(img)
+            img = crop_transform(img)
+            img = pil_to_tensor(img)
+            images.append(img)
+        
+        images = torch.stack(images)
+        return images, captions
+
+class BucketDataset(IterableDataset):
+    def __init__(self, 
+                 dataset,
+                 batch_size,
+                 aspect_ratios,
+                 discard_low_res=True):
+        super().__init__()
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.aspect_ratios = aspect_ratios
+        self.discard_low_res = discard_low_res
+    
+    def find_closest_ratio(self, img):
+        width = img.width
+        height = img.height
+        ratio = height / width
+
+        # find the closest ratio from the aspect ratios table
+        min_distance = 100
+        target_ratio = 0.6
+        for r in self.aspect_ratios.keys():
+            distance = abs(float(r) - ratio)
+            if min_distance > distance:
+                target_ratio = r
+                min_distance = distance
+        
+        # Return the result as a tuple (target_ratio, idx)
+        return str(target_ratio)
+
+    def __iter__(self):
+        # those are the left over images that didn't fit in any batch in the last epoch
+        left_overs = []
+        pil_to_tensor = PILToTensor()
+        # finally the buckets
+        buckets = {}
+        while True:
+            discarded_images = 0
+            for img, caption in self.dataset:
+                # calculate the closest aspect ratio for the image
+                ratio = self.find_closest_ratio(img)
+                height_target = int(self.aspect_ratios[ratio][0])
+                width_target = int(self.aspect_ratios[ratio][1])
+                width = img.width
+                height = img.height
+
+                # check if we discard this image due to a too low resolution
+                if height_target > height and width_target > width and self.discard_low_res:
+                    # don't push this image in any bucket
+                    discarded_images = discarded_images + 1
+                    continue
+
+                resize_transform = Resize((height_target, width_target))
+                img = resize_transform(img)
+
+                # find if this bucket already exists
+                if not ratio in buckets.keys():
+                    buckets[ratio] = []
+
+                    # also push a left over from the last epoch if any
+                    if len(left_overs) != 0:
+                        left_over = left_overs.pop()
+                        img, caption = left_over
+                        crop_transform = CenterCrop((height_target, width_target))
+                        img = crop_transform(img)
+                        buckets[ratio].append((img, caption))
+
+                # handle the case of batch size = 1???
+                if self.batch_size != 1:
+                    buckets[ratio].append((img, caption))
+
+                # check if any bucket is full
+                for key, bucket in buckets.items():
+                    if len(bucket) == self.batch_size:
+                        # transform the PIL image to a tensor
+                        images = []
+                        captions = []
+                        for elem in bucket:
+                            img, caption = elem
+                            images.append(pil_to_tensor(img))
+                            captions.append(caption)
+                        yield torch.stack(images), captions
+                        buckets.pop(key)
+            # check for left overs
+            for key, bucket in enumerate(buckets):
+                if len(bucket) != 0:
+                    left_overs.extend(bucket)
+            
+            # print some statistics
+            if self.discard_low_res:
+                tqdm.write(f'There were {discarded_images} low resolution images in this epoch')
+            tqdm.write(f'There are {len(left_overs)} images from this epoch that did not fit in any bucket')
+
+            # finally reinitialize the buckets
+            buckets = {}
+            discarded_images = 0
---
+++ train.py
@@ -0,0 +1,229 @@
+import argparse
+import boto3
+from botocore.config import Config
+import webdataset as wds
+from bucket_sampler import BucketSampler, BatchCollater, get_bucket_indices, BucketDataset
+from torch.utils.data import DataLoader, Subset
+from diffusers.pipelines.pixart_alpha.pipeline_pixart_alpha import ASPECT_RATIO_256_BIN, ASPECT_RATIO_512_BIN, ASPECT_RATIO_1024_BIN
+from diffusers.pipelines.pixart_alpha.pipeline_pixart_sigma import ASPECT_RATIO_2048_BIN
+from accelerate import Accelerator
+from diffusers import SanaTransformer2DModel, DPMSolverMultistepScheduler, AutoencoderDC
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from torch.optim.adamw import AdamW
+from torch.utils.tensorboard import SummaryWriter
+from diffusers import SanaPAGPipeline
+import torch
+from tqdm import tqdm
+from torchvision.transforms import PILToTensor
+from diffusers.utils.torch_utils import randn_tensor
+import gc
+
+def flush():
+    gc.collect()
+    torch.cuda.empty_cache()
+
+def extract_embeddings(captions : list[str],
+                       pipe : SanaPAGPipeline):
+    prompt_embeds, prompt_attention_mask, negative_prompt_embeds, negative_prompt_attention_mask = \
+        pipe.encode_prompt(captions, do_classifier_free_guidance=False)
+    return prompt_embeds, prompt_attention_mask
+
+def extract_latents(images : torch.Tensor,
+                    pipe : SanaPAGPipeline):
+    image_processor = pipe.image_processor
+    images = image_processor.pil_to_numpy(images)
+    images = torch.tensor(images, device=pipe.device, dtype=pipe.dtype)
+    images = image_processor.preprocess(torch.squeeze(images, dim=0))
+    flush()
+
+    output = pipe.vae.encode(images.to(device=vae.device, dtype=vae.dtype))
+    return output.latent
+
+def validate(logger : SummaryWriter,
+             global_step: int,
+             pipe : SanaPAGPipeline,
+             validation_prompts : list[str]):
+    pil_to_tensor = PILToTensor()
+    idx = 0
+    generator=torch.Generator(device="cuda").manual_seed(42)
+    for prompt in tqdm(validation_prompts, desc='Generating validation images'):
+        image = pipe(
+            prompt=prompt,
+            guidance_scale=5.0,
+            num_inference_steps=20,
+            generator=generator,
+        )[0]
+        logger.add_image(f'validation/{idx}/{prompt}', pil_to_tensor(image[0]), global_step)
+        idx = idx + 1
+    
+    # save the transformer
+    transformer.save_pretrained(f'{global_step}')
+
+def optimize(logger : SummaryWriter,
+             global_step: int,
+             pipe : SanaPAGPipeline,
+             latents : torch.Tensor,
+             embeddings : torch.Tensor,
+             attention_mask : torch.Tensor,
+             optimizer : AdamW,
+             accelerator : Accelerator):
+    loss_fn = torch.nn.MSELoss()
+    noise = randn_tensor(latents.shape, device=pipe.device, dtype=pipe.dtype)
+    
+    scheduler = pipe.scheduler
+    timestep = torch.randint(0, scheduler.config.num_train_timesteps, (1,), device=pipe.device)
+    timesteps = timestep.expand(latents.shape[0])
+    noisy_latents = scheduler.add_noise(latents, noise, timesteps)
+    noisy_latents = scheduler.scale_model_input(noisy_latents, timesteps)
+
+    transformer = pipe.transformer
+    with accelerator.accumulate(transformer):
+        noise_pred = transformer(noisy_latents,
+                                 encoder_hidden_states=embeddings,
+                                 timestep=timesteps,
+                                 encoder_attention_mask=attention_mask).sample
+        loss = loss_fn(noise_pred.to(dtype=noise.dtype), noise)
+        accelerator.backward(loss)
+        optimizer.step()
+        optimizer.zero_grad()
+    logger.add_scalar('train/loss', loss.detach().item(), global_step)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--r2_endpoint', required=True, type=str)
+    parser.add_argument('--r2_access_key', required=True, type=str)
+    parser.add_argument('--r2_secret_key', required=True, type=str)
+    parser.add_argument('--r2_bucket_name', required=True, type=str)
+    parser.add_argument('--r2_tar_files', nargs='+', required=True, type=str)
+
+    parser.add_argument('--batch_size', required=True, type=int)
+    parser.add_argument('--num_processes', required=False, type=int, default=1)
+    parser.add_argument('--pretrained_model_path',
+                        required=False,
+                        type=str,
+                        default='Efficient-Large-Model/Sana_600M_512px_diffusers')
+    parser.add_argument('--learning_rate', required=False, type=float, default=1e-5)
+    parser.add_argument('--num_epochs', required=False, type=int, default=5)
+    parser.add_argument('--num_steps_per_validation', required=False, type=int, default=5000)
+    parser.add_argument('--validation_prompts', required=True, nargs='+', type=str)
+    args = parser.parse_args()
+
+    # cloudflare related arguments
+    r2_endpoint = args.r2_endpoint
+    r2_access_key = args.r2_access_key
+    r2_secret_key = args.r2_secret_key
+    r2_bucket_name = args.r2_bucket_name
+    r2_tar_files = args.r2_tar_files
+
+    # training parameters
+    batch_size = args.batch_size
+    num_processes = args.num_processes
+    pretrained_model_path = args.pretrained_model_path
+    learning_rate = args.learning_rate
+    num_epochs = args.num_epochs
+    num_steps_per_validation = args.num_steps_per_validation
+    validation_prompts = args.validation_prompts
+
+    # get the urls from the cloudflare bucket with the keys
+    session = boto3.Session(
+        aws_access_key_id=r2_access_key,
+        aws_secret_access_key=r2_secret_key
+    )
+    config = Config(signature_version='s3v4')
+    s3_client = session.client('s3', endpoint_url=r2_endpoint, config=config)
+
+    # urls with 1 week expiration
+    urls = [s3_client.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': r2_bucket_name, 'Key':tar_file},
+        ExpiresIn=604800
+    ) for tar_file in r2_tar_files]
+
+    # build the dataset
+    dataset = (
+        wds.WebDataset(urls)
+        .shuffle(1000)  # Shuffle across all shards
+        .decode("pil")  # Decode images as PIL objects
+        .to_tuple("jpg", "txt")  # Return image and text
+    )
+
+    pipe = SanaPAGPipeline.from_pretrained(pretrained_model_path).to(dtype=torch.bfloat16)
+
+    # SANA transformer
+    transformer = pipe.transformer
+
+    # scheduler
+    scheduler = pipe.scheduler
+
+    # vae
+    vae = pipe.vae
+    vae.train(False)
+
+    # tokenizer
+    tokenizer = pipe.tokenizer
+
+    # text encoder
+    text_encoder = pipe.text_encoder
+    text_encoder.train(False)
+
+    vae_compression = 32
+    resolution = transformer.sample_size * vae_compression
+    if resolution == 256:
+        aspect_ratio = ASPECT_RATIO_256_BIN
+    elif resolution == 512:
+        aspect_ratio = ASPECT_RATIO_512_BIN
+    elif resolution == 1024:
+        aspect_ratio = ASPECT_RATIO_1024_BIN
+    else:
+        aspect_ratio = ASPECT_RATIO_2048_BIN
+    bucket_dataset = BucketDataset(dataset, batch_size, aspect_ratio)
+
+    # batch collater that applies a transformation
+    def collate_fn(samples):
+        # Unpack individual samples into images and captions
+        images = torch.stack([sample[0][0] for sample in samples])  # List of PIL images
+        captions = [sample[1] for sample in samples]  # List of strings (captions)
+        return images, captions
+    
+    # optimizer
+    optimizer = AdamW(transformer.parameters(), lr=learning_rate)
+    dataloader = DataLoader(bucket_dataset, batch_size=None)
+
+    # multi-gpu training
+    accelerator = Accelerator(mixed_precision='bf16')
+    transformer, scheduler, vae, tokenizer, text_encoder, optimizer = accelerator.prepare(
+        transformer, scheduler, vae, tokenizer, text_encoder, optimizer
+    )
+    global_step = 0
+
+    if accelerator.is_main_process:
+        logger = SummaryWriter()
+    
+    for epoch in range(num_epochs):
+        for images, captions in tqdm(dataloader):
+            images = torch.squeeze(images, dim=0)
+            if global_step % num_steps_per_validation == 0:
+                if accelerator.is_main_process:
+                    with torch.no_grad():
+                        validate(logger,
+                                global_step,
+                                pipe,
+                                validation_prompts)
+                accelerator.wait_for_everyone()
+
+            with torch.no_grad():
+                embeddings, attention_mask = extract_embeddings(captions, pipe)
+                latents = extract_latents(images, pipe)
+            
+            optimize(logger,
+                     global_step,
+                     pipe,
+                     latents,
+                     embeddings,
+                     attention_mask,
+                     optimizer,
+                     accelerator)
+            global_step = global_step + 1
+
+
+
