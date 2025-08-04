2fb789342b519a71cbd1ee04614c57a9b5980666
Lucas Malo Belanger <lucas.malo.belanger@gmail.com>
2025-08-03 20:25:34 -0400

    Speed up fetching with image batching

--- common/dataset_fetcher.py
+++ common/dataset_fetcher.py
@@ -30,7 +30,8 @@ class DatasetFetcher:
             target_height = int(aspect_ratio[0])
             target_width = int(aspect_ratio[1])
             return transforms.Compose([
-                transforms.Resize((target_height, target_width))
+                transforms.Resize((target_height, target_width)),
+                transforms.ToTensor()
             ])
             
         while True:
@@ -46,46 +47,44 @@ class DatasetFetcher:
                 w, h = img.size
                 return self.model.find_closest_ratio(h / w)
 
-            def with_bucket(sample):
-                image, caption = sample
-                bucket = assign_bucket(image)
-                return {"jpg": image, "txt": caption, "bucket": bucket}
-
-
-
             dataset = (
                 wds.WebDataset(dataset_url, shardshuffle=False, nodesplitter=None, workersplitter=None)
-                .shuffle(1000)
                 .decode('pil')
+                .batched(16)
                 .to_tuple('jpg', 'txt')
-                .map(with_bucket)
             )
             loader = wds.WebLoader(dataset, batch_size=None, num_workers=4)
 
             to_tensor = transforms.Compose([
                 transforms.ToTensor()
             ])
-            for sample in loader:
-                bucket = sample["bucket"]
-                if bucket not in self.queues:
-                    self.queues[bucket] = []
+            for batch in loader:
+                images = batch[0]
+                captions = batch[1]
+                for idx in range(len(images)):
+                    img = images[idx]
+                    caption = captions[idx]
 
-                # Apply transform
-                sample["jpg"] = to_tensor(sample["jpg"])
-                self.queues[bucket].append(sample)
+                    # Apply transform
+                    w, h = img.size
+                    bucket = self.model.find_closest_ratio(h / w)
 
-                if len(self.queues[bucket]) >= self.batch_size:
-                    batch = self.queues[bucket][:self.batch_size]
-                    self.queues[bucket] = []
-                    
-                    transform = get_transform(bucket)
-                    images = torch.stack([transform(x["jpg"]) for x in batch])
-                    captions = [x["txt"] for x in batch]
+                    if bucket not in self.queues:
+                        self.queues[bucket] = []
+                    self.queues[bucket].append((img, caption))
 
-                    # Feature extraction (example)
-                    # features = self.model.extract_features(images)
-                    # yield features, captions
+                    if len(self.queues[bucket]) >= self.batch_size:
+                        batch = self.queues[bucket][:self.batch_size]
+                        self.queues[bucket] = []
+                        
+                        transform = get_transform(bucket)
+                        batch_images = torch.stack([transform(x[0]) for x in batch])
+                        batch_captions = [x[1] for x in batch]
 
-                    yield images, captions, bucket
+                        # Feature extraction (example)
+                        # features = self.model.extract_features(images)
+                        # yield features, captions
+
+                        yield batch_images, batch_captions, bucket
 
             self.current_shard_index = (self.current_shard_index + 1) % self.num_shards
