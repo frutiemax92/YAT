13442e7059dfad974af07bf989b09fb0b445427d
Lucas Malo Belanger <lucas.malo.belanger@gmail.com>
2025-10-19 19:22:29 -0400

    Add total timeout on file download

--- common/cloudflare.py
+++ common/cloudflare.py
@@ -2,6 +2,7 @@ import boto3
 from botocore.config import Config
 import requests
 from tqdm import tqdm
+import time
 
 def get_client(r2_access_key, r2_secret_key, r2_endpoint):
     session = boto3.Session(
@@ -32,10 +33,17 @@ def get_secured_urls(r2_access_key : str,
         ExpiresIn=604800
     ) for tar_file in r2_tar_files]
 
-def download_tar(url, local_path):
+def download_tar(url, local_path, max_total_time=60):
+    start_time = time.time()
+
     # if we don't get any chunk under 60 seconds, raise an exception!
-    with requests.get(url, stream=True, timeout=60) as r:
+    with requests.get(url, stream=True, timeout=240) as r:
         r.raise_for_status()
         with open(local_path, 'wb') as f:
             for chunk in tqdm(r.iter_content(chunk_size=8192), desc='downloading chunks'):
                 f.write(chunk)
+
+                if time.time() - start_time > max_total_time:
+                    raise DownloadTimeoutError(
+                        f"Total download time exceeded {max_total_time} seconds"
+                    )
